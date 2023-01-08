44270a5d74f7035912843e31163b0cb216fccfbd
MalayVyas <106895969+MalayVyas@users.noreply.github.com>
2023-01-08 16:26:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if st.button("Rate the Chances"):
     inputs = np.expand_dims([input_marks_GRE, input_marks_TOEFL,
                             input_Uni_Rating, input_SOP, input_LOR, input_CGPA,research], 0)
     prediction = loaded_model.predict(inputs)
+    print(type(prediction))
 st.write("Your chances are " + str(prediction*100) + "%")
-print(type(prediction))
 if st.button("NOTE:"):
   st.write("This model is 97% accurate so there may be some exceptions for the admissions")
